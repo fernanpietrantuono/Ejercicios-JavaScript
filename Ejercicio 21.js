410f2c4d1863569a1645fef6e5663d7dcbd1bda7
Fernan Pietrantuono <fernanpietrantuono@gmail.com>
2023-08-14 17:18:24 -0300

    Cuarto Commit

--- Ejercicio 21.js
+++ Ejercicio 21.js
@@ -16,12 +16,11 @@ function cargarPersona() {
     let sexo = formulario.elements["sexo"].value;
     let altura = formulario.elements["altura"].value;
     let peso = formulario.elements["peso"].value;
-    console.log("Nombre: " + nombre.trim() + "\nEdad: " + edad.trim() + "\nSexo: " + sexo.trim() + "\nAltura: " + altura.trim() + "\nPeso: " + peso.trim());
     return new Persona(nombre, edad, sexo, altura, peso);
 }
 
-formulario.onsubmit = (evento) => {
-    evento.preventDefault();
+formulario.onsubmit = (event) => {
+    event.preventDefault();
     let persona = cargarPersona();
     nombrePersona.textContent = persona.nombre;
     edadPersona.textContent = persona.edad;
@@ -29,3 +28,14 @@ formulario.onsubmit = (evento) => {
     alturaPersona.textContent = persona.altura;
     pesoPersona.textContent = persona.peso;
 };
+
+function ejecutarAlPresionarEnter(event) {
+    if (event.keyCode === 13) {
+        let persona = cargarPersona();
+        nombrePersona.textContent = persona.nombre;
+        edadPersona.textContent = persona.edad;
+        sexoPersona.textContent = persona.sexo;
+        alturaPersona.textContent = persona.altura;
+        pesoPersona.textContent = persona.peso;    
+    }
+}
